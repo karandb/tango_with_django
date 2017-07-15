444ec540b1e15ac58cd7740a80da9109a918bfad
Karandeep Baweja <karan@Karans-Mac.local>
2017-07-15 15:19:50 +0100

    this is the second commit

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Motu Karan says hi!")
+
+    context_dict = { 'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = { 'boldmessage': "Karan Baweja"}
+
+    return render(request, 'rango/about.html', context=context_dict)
